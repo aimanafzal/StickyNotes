c6b6c6d2b597a9eb6cf79d766c4d17232dbfdbbb
aimanafzal <ayman.afzal@msn.com>
2019-09-14 09:56:08 +0500

    Update mock.js

--- demo/src/components/mock.js
+++ demo/src/components/mock.js
@@ -1,7 +1,7 @@
 const data = [
   {
     "id": "3effea2c-fc90-98e0-51d0-22c32efb2177",
-    "text": "Hey I am Ajain... ",
+    "text": "Mock Data",
     "title": "Hello",
     "grid": {
       "i": "3effea2c-fc90-98e0-51d0-22c32efb2177",
@@ -9,21 +9,6 @@ const data = [
       "y": null,
       "w": 2,
       "h" : 2,
-      "isDraggable": false
-    },
-    "contentEditable": true,
-    "timeStamp": "13 Feb 2017 2:53 PM"
-  },
-  {
-    "id": "3effea2c-fc90-98e0-51d0-22c32efb2178",
-    "text": "If you liked stickies... contribute by liking it... ",
-    "title": "Contribute",
-    "grid": {
-      "i": "3effea2c-fc90-98e0-51d0-22c32efb2178",
-      "x": 4,
-      "y": 0,
-      "w": 4,
-      "h" : 4,
       "isDraggable": false
     },
     "contentEditable": true,
