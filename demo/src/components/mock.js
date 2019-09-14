2495b72c7e53fd139342bff9286839bee37f548d
aimanafzal <ayman.afzal@msn.com>
2019-09-14 10:02:54 +0500

    Add files via upload

--- demo/src/components/mock.js
+++ demo/src/components/mock.js
@@ -1,19 +1,17 @@
-const data = [
-  {
+const data = [{
     "id": "3effea2c-fc90-98e0-51d0-22c32efb2177",
     "text": "Mock Data",
     "title": "Hello",
     "grid": {
-      "i": "3effea2c-fc90-98e0-51d0-22c32efb2177",
-      "x": 0,
-      "y": null,
-      "w": 2,
-      "h" : 2,
-      "isDraggable": false
+        "i": "3effea2c-fc90-98e0-51d0-22c32efb2177",
+        "x": 0,
+        "y": null,
+        "w": 2,
+        "h": 2,
+        "isDraggable": false
     },
     "contentEditable": true,
     "timeStamp": "13 Feb 2017 2:53 PM"
-  }
-]
+}]
 
 export default data;
